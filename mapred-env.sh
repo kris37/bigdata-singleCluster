cdf6ba08660e20c708db74d44ca8afa2ef8270b8
Qiang.Pan <qiang.pan@qyer.com>
2017-04-10 18:22:44 +0800

    add logs and mapreduce env homeetc

--- mapred-env.sh
+++ mapred-env.sh
@@ -20,6 +20,8 @@ export HADOOP_JOB_HISTORYSERVER_HEAPSIZE=1000
 export HADOOP_MAPRED_ROOT_LOGGER=INFO,RFA
 
 #export HADOOP_JOB_HISTORYSERVER_OPTS=
+export HADOOP_MAPRED_LOG_DIR=${HADOOP_MAPRED_LOG_DIR}
+export HADOOP_MAPRED_PID_DIR=${HADOOP_MAPRED_PID_DIR}
 #export HADOOP_MAPRED_LOG_DIR="" # Where log files are stored.  $HADOOP_MAPRED_HOME/logs by default.
 #export HADOOP_JHS_LOGGER=INFO,RFA # Hadoop JobSummary logger.
 #export HADOOP_MAPRED_PID_DIR= # The pid files are stored. /tmp by default.
